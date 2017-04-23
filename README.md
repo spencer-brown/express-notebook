b955f340645b4333684b00dba4a9c5ddce24e4e9
Spencer Brown <me@spencer.sx>
2017-04-22 22:12:50 -0700

    Add initial code

---
+++ README.md
@@ -0,0 +1,85 @@
+# express-notebook
+
+A simple, drop-in markdown blogging framework for Express applications.
+
+* Write blog posts in markdown.
+* Template-engine-agnostic.
+* Simple setup and configuration.
+* Drops into existing applications.
+
+## Installation
+
+```sh
+$ yarn add express-notebook
+```
+or
+```sh
+$ npm install express-notebook --save
+```
+
+## Usage
+
+1. Register `express-notebook` with your application.
+
+```js
+const Notebook = require('express-notebook');
+
+const notebook = new Notebook({
+  // Your Express app object.
+  app,
+
+  /**
+   * The directory in your project where you'll keep your post markdown files and `data.json`
+   * (explained below).
+   */
+  postsDir: 'src/posts',
+
+  // The URL path prefix where your posts will be accessed (eg example.com/posts/foo-bar).
+  urlPath: '/posts',
+
+  /**
+   * The template with which your posts will be rendered. Should render a `post` variable that's
+   * rendered unescaped since `post` will be an HTML string.
+   */
+  template: 'post'
+});
+
+notebook.setPostRoutes();
+```
+
+2. Create a directory for your posts somewhere in your project. In this directory, add a file called `data.json` with your post data like this:
+
+eg, `src/posts/data.json`
+```json
+{
+  "posts": [{
+
+    // The name of the markdown file corresponding to this post. This markdown file should live in the same directory as data.json
+    "filename": "foo.md",
+
+    // The URL slug for this post.
+    "slug": "my-blog-post",
+
+    // Any metadata want to pass to your post template.
+    "metadata": {
+      "title": "My blog post title",
+      "date": "3/14/2017",
+      "location": "San Francisco, California"
+    }
+  }, {
+    ...
+  }]
+}
+```
+
+`express-notebook` also exposes the post data read from `data.json`. This is useful for creating a post index, for example.
+
+```js
+...
+const posts = notebook.getPostData();
+
+res.render('/posts', {
+  posts
+});
+...
+```
---
+++ src/index.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const marked = require('marked');
+
+
+class ExpressNotebook {
+
+  /**
+   * @param {Object} options
+   *  @param {Object} [app] - An Express app object.
+   *  @param {String} postsDir - A directory relative to your project root directory that contains
+   *  your markdown post files and a data.json file.
+   *  @param {String} [urlPath] - The URL path where your posts will be available. Setting this to
+   *  '/posts' will make a post with slug 'foo' available at '/posts/foo'.
+   *  @param {String} [template] - The template with which your post will be rendered. Variables
+   *  `post` and `metadata` will be rendered as locals to this template. The `post` local variable
+   *  should be rendered unescaped.
+   */
+  constructor({ app, postsDir, urlPath, template }) {
+    this._app = app;
+    this._postsDir = postsDir;
+    this._urlPath = urlPath;
+    this._template = template;
+
+    this._loadData();
+  }
+
+  setPostRoutes() {
+    this._loadPosts();
+
+    this._setMiddleware();
+  }
+
+  /**
+   * Reads data.json
+   */
+  _loadData() {
+    const dataStr = fs.readFileSync(`${this._postsDir}/data.json`, 'utf8');
+    this._data = JSON.parse(dataStr);
+  }
+
+  /**
+   * Reads markdown posts files and converts them to HTML. Done once at startup so that we can cache
+   * the posts in memory and minimize time spent reading from disk.
+   */
+  _loadPosts() {
+    this._loadedPosts = {};
+
+    this._data.posts.forEach((post) => {
+      const markdown = fs.readFileSync(`${this._postsDir}/${post.filename}`, 'utf8');
+
+      this._loadedPosts[post.slug] = marked(markdown);
+    });
+  }
+
+  /**
+   * Sets GET handlers for each of the posts that renders the configured template with the post and
+   * its metadata.
+   */
+  _setMiddleware() {
+    this._data.posts.forEach((post) => {
+
+      // Register the route for this post.
+      this._app.get(`${this._urlPath}/${post.slug}`, (req, res) => {
+        res.render(this._template, {
+          post: this._loadedPosts[post.slug],
+          metadata: post.metadata
+        });
+      });
+    });
+  }
+
+  /**
+   * Returns the array of posts read in from `data.json`. Useful for retrieving data to be used to
+   * render an index of posts, for example.
+   */
+  getPostData() {
+    return this._data.posts;
+  }
+}
+
+module.exports = ExpressNotebook;
